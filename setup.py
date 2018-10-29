a269adf8ba053a6abad3731616547d736019f53c
rozen03 <urielrozenberg@hotmail.com>
2018-10-29 19:07:04 -0300

    setop

--- setup.py
+++ setup.py
@@ -19,42 +19,42 @@ def requirements():
 
 packages = find_packages(exclude=['tests*'])
 
-with codecs.open('README.rst', 'r', 'utf-8') as fd:
-    fn = os.path.join('telegram', 'version.py')
-    with open(fn) as fh:
-        code = compile(fh.read(), fn, 'exec')
-        exec(code)
+# with codecs.open('README.rst', 'r', 'utf-8') as fd:
+# fn = os.path.join('telegram', 'version.py')
+# with open(fn) as fh:
+# code = compile(fh.read(), fn, 'exec')
+# exec(code)
 
-    setup(name='python-telegram-bot',
-          version=__version__,
-          author='Rozen',
-          # author_email='devs@python-telegram-bot.org',
-          license='LGPLv3',
-          # url='https://python-telegram-bot.org/',
-          # keywords='python telegram bot api wrapper',
-          description="Cosa",
-          long_description=fd.read(),
-          packages=packages,
-          install_requires=requirements(),
-          extras_require={
+setup(name='ArgenFecha',
+      version=__version__,
+      author='Rozen',
+      # author_email='devs@python-telegram-bot.org',
+      license='LGPLv3',
+      # url='https://python-telegram-bot.org/',
+      # keywords='python telegram bot api wrapper',
+      description="Cosa",
+      long_description=fd.read(),
+      packages=packages,
+      install_requires=requirements(),
+      extras_require={
               # 'json': 'ujson',
               # 'socks': 'PySocks'
-          },
-          include_package_data=True,
-          classifiers=[
-              'Development Status :: 5 - Production/Stable',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-              'Operating System :: OS Independent',
-              'Topic :: Software Development :: Libraries :: Python Modules',
-              # 'Topic :: Communications :: Chat',
-              # 'Topic :: Internet',
-              'Programming Language :: Python',
-              'Programming Language :: Python :: 2',
-              'Programming Language :: Python :: 2.7',
-              'Programming Language :: Python :: 3',
-              'Programming Language :: Python :: 3.4',
-              'Programming Language :: Python :: 3.5',
-              'Programming Language :: Python :: 3.6'
-              'Programming Language :: Python :: 3.7'
-          ],)
+      },
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          # 'Topic :: Communications :: Chat',
+          # 'Topic :: Internet',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 3.7'
+      ],)
